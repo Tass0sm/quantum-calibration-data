c816a9c8df73f3a8a296dddcadf974c63148549e
Tassos Manganaris <tassos.manganaris@gmail.com>
2023-03-31 10:27:57 -0400

    Added initial files

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd /home/tassos/work/quantum/calibration-data/
+source /home/tassos/software/calibration-fetcher-2/selenium-venv/bin/activate
+python /home/tassos/software/calibration-fetcher-2/main.py
